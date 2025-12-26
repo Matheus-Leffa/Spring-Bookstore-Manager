445cfef8b35f3f671a5ee946c5d7e72cfb31bfc3
Matheus-Leffa <matheusleffa@live.com>
2025-12-26 10:27:33 -0300

    Developed project entities

---
+++ src/main/java/com/matheusleffa/bookstoremanager/Entity/Author.java
@@ -0,0 +1,24 @@
+package com.matheusleffa.bookstoremanager.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer age;
+
+}
