708dbab002f0e1ea090889fde435b152678899a3
Matheus-Leffa <matheusleffa@live.com>
2025-12-27 14:50:34 -0300

    Developed get by id operation

--- src/main/java/com/matheusleffa/bookstoremanager/controller/BookController.java
+++ src/main/java/com/matheusleffa/bookstoremanager/controller/BookController.java
@@ -7,10 +7,7 @@ import com.matheusleffa.bookstoremanager.repository.BookRepository;
 import com.matheusleffa.bookstoremanager.service.BookService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/books")
@@ -26,5 +23,10 @@ public class BookController {
     @PostMapping
     public MessageResponseDTO create(@RequestBody @Valid BookDTO bookDTO){
         return bookService.create(bookDTO);
+    }
+
+    @GetMapping("/{id}")
+    public BookDTO findById(@PathVariable Long id){
+        return bookService.findById(id);
     }
 }
